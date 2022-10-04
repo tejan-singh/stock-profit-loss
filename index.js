6a9d947863c4fa6a0a5b79b129fbdbd041c1dabd
Tejan Singh <tstejan@gmail.com>
2022-10-04 21:01:00 +0530

    feat: added functionality to calculate profit/loss

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-const initialPriceInput = document.querySelector('.initial-price')
-const currentPriceInput = document.querySelector('.current-price')
-const noOfStocksInput = document.querySelector('.no-of-stocks')
-const calculateBtn = document.querySelector('.btn')
-const resultMsg = document.querySelector('#result')
+const initialPriceInput = document.querySelector(".initial-price");
+const currentPriceInput = document.querySelector(".current-price");
+const noOfStocksInput = document.querySelector(".no-of-stocks");
+const calculateBtn = document.querySelector(".btn");
+const resultMsg = document.querySelector("#result");
+
+const handleClick = () => {
+  const initialPrice = Number(initialPriceInput.value);
+  const currentPrice = Number(currentPriceInput.value);
+  const noOfStocks = Number(noOfStocksInput.value);
+  const priceDiff = currentPrice - initialPrice;
+  const value = priceDiff * noOfStocks;
+  const percentageChange = ((priceDiff / initialPrice) * 100).toFixed(2)
+  
+
+  console.log(initialPrice, currentPrice, noOfStocks);
+  if (initialPrice > 0 && currentPrice >= 0 && noOfStocks > 0) {
+   
+    if (currentPrice > initialPrice) {
+
+      resultMsg.style.color = "green";
+      resultMsg.innerHTML = `Your profit is ${value} and percentage profit is ${percentageChange}%`;
+      return;
+    }
+
+    if (currentPrice < initialPrice) {
+
+      resultMsg.style.color = "red";
+      resultMsg.innerHTML = `Your profit is ${value} and percentage profit is ${percentageChange}%`;
+      return;
+    }
+
+    if (currentPrice === initialPrice) {
+
+      resultMsg.style.color = "blue";
+      resultMsg.innerHTML = `No Gain! No Pain!`;
+      return;
+    }
+  }
+
+
+  resultMsg.style.color = "red";
+  resultMsg.innerHTML = "Please enter correct values";
+};
+
+calculateBtn.addEventListener("click", handleClick);
